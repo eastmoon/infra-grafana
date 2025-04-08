5498ef11c29ccea650e6a5620a3109fabb234b8c
Jacky Eastmoon <jacky_eastmoon@hotmail.com>
2025-04-08 18:03:57 +0800

    refactor: abstract get authorization script

--- shell/test-team.sh
+++ shell/test-team.sh
@@ -5,22 +5,7 @@ api() {
 ## Execute initial grafana server admin account
 echo "----- Team operation -----"
 ### Create auth-token
-saname=Admin-Demo
-satoken=Admin-Token
-sarole=Admin
-satmpfile=/tmp/satoken
-if [ -z $(api sys auth.token) ]; then
-    echo "----- Auth create -----"
-    [ $(api sa ls | grep ${saname} | wc -l) -eq 0 ] && api sa create --name=${saname} --role=${sarole} || true
-    said=$(api sa ls | grep ${saname} | tr ',' '\n' | awk '$0 ~ /"id"/ { split( $0, a, ":" ); print(a[2]) }')
-    if [ $(api sa token.ls --id=${said} | grep ${satoken} | wc -l) -gt 0 ]; then
-        satid=$(api sa token.ls --id=${said} | grep ${satoken} | tr ',' '\n' | awk '$0 ~ /"id"/ { split( $0, a, ":" ); print(a[2]) }')
-        api sa token.rm --id=${said} --tid=${satid}
-    fi
-    api sa token.create --id=${said} --name=${satoken} | tee ${satmpfile}
-    api sys auth.token.save --file=${satmpfile}
-    echo "----- Auth create END -----"
-fi
+source tool-get-auth.sh
 ### Show all team
 api team ls
 ### Create new team and remove it
